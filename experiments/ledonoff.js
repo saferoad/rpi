f19e971c4508fa4691d45595633d272cc5955743
Nick Chikovani <nikabika97@gmail.com>
2015-12-15 22:19:14 +0400

    added led init emit

--- experiments/ledonoff.js
+++ experiments/ledonoff.js
@@ -5,11 +5,15 @@ led = new Gpio(5, 'out');
 
 socket.on('connect', function() {
 	console.log("Connected to server");
+	socket.emit('led.init', {});
+})
 
 socket.on('led.update', function(data) {
 	if(data.power) {
+		console.log("Turning led ON");
 		led.writeSync(1)
 	} else {
+		console.log("Turning led OFF");
 		led.writeSync(0)
 	}
 })
