946205a578b4fb18663107b265441ab2bbf08463
Nick Chikovani <nikabika97@gmail.com>
2015-12-16 23:46:20 +0400

    added distance-test

---
+++ experiments/distance-test.js
@@ -0,0 +1,43 @@
+var statistics = require('math-statistics');
+var usonic = require('r-pi-usonic');
+
+var init = function(config) {
+	var sensor = usonic.sensor(config.echoPin, config.triggerPin, config.timeout);
+	//console.log(config);
+	var distances;
+	(function measure() {
+		if (!distances || distances.length === config.rate) {
+			if (distances) {
+				print(distances);
+			}
+			
+			distances = [];
+		}
+
+		setTimeout(function() {
+			distances.push(sensor());
+			measure();
+		}, config.delay);
+	}());
+};
+
+var print = function(distances) {
+	
+	var distance = statistics.median(distances);
+	process.stdout.clearLine();
+	process.stdout.cursorTo(0);
+	
+	if (distance < 0) {
+		process.stdout.write('Error: Measurement timeout.\n');
+	} else {
+		process.stdout.write('Distance: ' + distance.toFixed(2) + ' cm');
+	}
+};
+
+init({
+	echoPin: 18, //Echo pin
+	triggerPin: 17, //Trigger pin
+	timeout: 1000, //Measurement timeout in µs
+	delay: 60, //Measurement delay in ms
+	rate: 5 //Measurements per sample
+});
