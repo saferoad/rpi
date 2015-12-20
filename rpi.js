cf5ddb00fad7d3f332e04356fe90969e5dd6305d
Nick Chikovani <nikabika97@gmail.com>
2015-12-20 10:34:51 +0400

    fix

--- rpi.js
+++ rpi.js
@@ -26,10 +26,11 @@ fs.readFile(__dirname+'/config.env', function(err, data){
 init = function(){
 	Gpio = require('onoff').Gpio;
 	led = new Gpio(3, 'out');
-	socket = require('socket.io-client')(config.server);
+	socket = require('socket.io-client')("http://45.55.220.113");
 	radar = usonic.createSensor(23, 24, 1000);
 
 	socket.on("light.up", function(data) {
+		console.log("light-up");
 		if(led.gpio.readSync() == 0) {
 			led.gpio.writeSync(1);
 			setTimeout(function() {
