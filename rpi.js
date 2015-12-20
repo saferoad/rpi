2163eaa2b31c2f6e34743e8fe8f79e09f7bcd039
Nick Chikovani <nikabika97@gmail.com>
2015-12-20 13:34:34 +0400

    fix

--- rpi.js
+++ rpi.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 var usonic = require('r-pi-usonic');
-var socket = require('socket.io-client')("http://45.55.220.113");
+var io = require('socket.io-client')("http://45.55.220.113");
 
 var Gpio = null;
 var socket = null;
@@ -9,51 +9,35 @@ var radar = null;
 var led = null;
 
 
-socket.on('connect', function() {
+io.on('connect', function(socket) {
 	console.log("Connected!");
 
+
+	io.on("light.up", function(data) {
+		console.log("light-up");
+		if(led.gpio.readSync() == 0) {
+			led.gpio.writeSync(1);
+			setTimeout(function() {
+				led.gpio.writeSync(0);
+			}, 2000);
+		}
+	});
 });
 
-socket.on("light.up", function(data) {
-	console.log("light-up");
-	if(led.gpio.readSync() == 0) {
-		led.gpio.writeSync(1);
-		setTimeout(function() {
-			led.gpio.writeSync(0);
-		}, 2000);
-	}
-});
+init = function(){
+	Gpio = require('onoff').Gpio;
+	led = new Gpio(2, 'out');
+	radar = usonic.createSensor(3, 4, 1000);
 
-// fs.readFile(__dirname+'/config.env', function(err, data){
-// 	if(err) {
-// 		throw err;
-// 	}
+	monitorRadar();
+}
 
-// 	config = JSON.parse(data);
-// 	usonic.init(function(err){
-// 		if(err) {
-// 			console.log("Error: "+err);
-// 		} else {
-// 			init();
-// 		}
-// 	});
-// });
-
-
-// init = function(){
-// 	Gpio = require('onoff').Gpio;
-// 	led = new Gpio(3, 'out');
-// 	radar = usonic.createSensor(23, 24, 1000);
-
-// 	monitorRadar();
-// }
-
-// monitorRadar  = function() {
-// 	setInterval(function() {
-// 		var distance = radar();
-// 		console.log(distance.toFixed(2)+"cm");
-// 		if(distance > 0 && distance < 10) {
-// 			socket.emit("capture.car",{});
-// 		}
-// 	}, 100)
-// }
+monitorRadar  = function() {
+	setInterval(function() {
+		var distance = radar();
+		console.log(distance.toFixed(2)+"cm");
+		if(distance > 0 && distance < 10) {
+			socket.emit("capture.car",{});
+		}
+	}, 100)
+}
